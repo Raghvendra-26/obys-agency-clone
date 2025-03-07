f74ab068b2f0065bcb51790295e1ed072bf80b9a
Raghvendra Singh <raghvendra.21cse270@rtu.ac.in>
2025-03-07 11:59:34 +0530

    page 2 and 4 done

--- script.js
+++ script.js
@@ -13,59 +13,66 @@ function loadingAnimation(){
         onStart: function(){
             var h5timer = document.querySelector("#line1-part1 h5")
             var grow = 0
-            setInterval(() => {
-                if(grow<100){
-                    h5timer.innerHTML = grow++
-                }
-                else{
-                    h5timer.innerHTML = grow
+            let interval = setInterval(() => {
+                if(grow < 100){
+                    h5timer.innerHTML = grow++;
+                } else {
+                    h5timer.innerHTML = grow;
+                    clearInterval(interval);  // Stop the timer at 100
                 }
             }, 30);
         }
-    })
+    });
 
     tl.to('.line h2',{
         animationName: "anime",
         opacity: 1
-    })
+    });
 
     tl.to('#loader',{
         opacity: 0,
         duration: 0.2,
         delay: 3.2
-    })
+    });
 
     tl.from("#page1",{
         delay:0.2,
-        y:1600,
+        yPercent:100,
         opacity:0,
         duration:0.4,
-        ease: Power4
-    })
+        ease: Power4.easeOut
+    });
 
     tl.to("#loader",{
         display: "none"
-    })
+    });
 
     tl.from("#nav",{
         opacity:0
-    })
+    });
 
     tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero3 h3,#hero4 h1",{
-        y:120,
+        y:140,
         stagger: 0.1
-    })
-}
-function cursorAnimation(){
-    document.addEventListener("mousemove",function(dets){
-        gsap.to("#crsr",{
-            left: dets.x,
-            top: dets.y
-        })
-    })
-    
-    Shery.makeMagnet("#nav-part2 h4");
+    });
 }
 
-loadingAnimation()
-cursorAnimation()
+function cursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr",{
+            left: dets.x,
+            top: dets.y,
+            duration: 0.2,  // Smooth movement
+            ease: "power2.out"
+        });
+    });
+
+    if (typeof Shery !== "undefined" && Shery.makeMagnet) {
+        Shery.makeMagnet("#nav-part2 h4");
+    } else {
+        console.warn("Shery library is missing!");
+    }
+}
+
+loadingAnimation();
+cursorAnimation();
